ca387f518ea1ad9cc3f794d138b6b552ffac87e5
Greg James <gregvjames99@gmail.com>
2026-04-11 21:42:44 -0500

    Add test for bulk insert

--- src/HyperTrieCore.Tests/TrieTests.cs
+++ src/HyperTrieCore.Tests/TrieTests.cs
@@ -16,6 +16,17 @@ public class TrieTests
         Assert.True(trie.Contains("banana"));
         Assert.False(trie.Contains("orange"));
     }
+    
+    [Fact]
+    public void TestBulkInsertion()
+    {
+        using var trie = new TrieNative(4, 3);
+        trie.BulkInsert(["apple", "banana"]);
+
+        Assert.True(trie.Contains("apple"));
+        Assert.True(trie.Contains("banana"));
+        Assert.False(trie.Contains("orange"));
+    }
 
     [Fact]
     public void TestPrefixSearch()
@@ -24,7 +35,7 @@ public class TrieTests
         trie.Insert("apple");
         trie.Insert("app");
         trie.Insert("application");
-
+        
         var results = trie.GetWordsWithPrefix("app");
         
         Assert.Equal(3, results.Count);
